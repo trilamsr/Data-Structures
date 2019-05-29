f5a0d0e8604990f2486ee44042e784cc7f416711
trilamsr <trilam153@gmail.com>
2019-05-28 23:44:47 -0700

    SetBuiltIn

--- Set implementation.js
+++ Set implementation.js
@@ -1,37 +1,93 @@
-const NO_DATA = 'NO DATA'
-
 class SetBuiltIn {
-    constructor (dataArray) {
-        this.storage = [];
-        this.length = 0;
-        (function () {
-            for (let ele of dataArray) {
-                if (this.storage.includes(ele)) {continue;}
-                this.storage.push(ele)
-            } 
-        })()
-    }
-    add (data) {
-        this.storage.push(data);
-        this.length ++;
-    }
-    remove (data) {
-        let index = this.storage.indexOf(data)
-        if (index > -1) {
-            this.length --;
-            return this.storage.splice(index, 1);
-        } else { 
-            return NO_DATA
+  constructor(dataArray) {
+    this.storage = [];
+    this.length = 0;
+    (() => {
+      for (let ele of dataArray) {
+        if (this.storage.includes(ele)) {
+          continue;
         }
+        this.storage.push(ele);
+      }
+      this.length = this.storage.length;
+    })();
+  }
+  show() {
+    return this.storage;
+  }
+  add(data) {
+    if (this.storage.includes(data)) {
+      throw new Error("The set already has this data");
+    } else {
+      this.storage.push(data);
+      this.length++;
     }
-    size () {
-        return this.length;
+  }
+  remove(data) {
+    if (this.storage.includes(data)) {
+      let index = this.storage.indexOf(data);
+      this.length--;
+      return this.storage.splice(index, 1);
+    } else {
+      return "Set does not have this data";
     }
-    union (setObject) {
-        return new SetBuiltIn ([...this.storage, ...setObject.storage])
+  }
+  size() {
+    return this.length;
+  }
+  union(set) {
+    if (set instanceof SetBuiltIn) {
+      console.log(
+        "Union return a new instance of set with data from both sets"
+      );
+      return new SetBuiltIn([...set.storage, ...this.storage]);
+    } else {
+      throw new Error("This set is not an instance of SetBuiltIn");
     }
-    intersect () {}
-    subset () {}
-    difference () {}
-    show () {}
+  }
+  subsetOf(set) {
+    if (set instanceof SetBuiltIn) {
+      for (let data of this.storage) {
+        if (!set.storage.includes(data)) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      throw new Error("This set is not an instance of SetBuiltIn");
+    }
+  }
+
+  intersect(set) {
+    if (set instanceof SetBuiltIn) {
+      console.log(
+        "Intersect return a new instance of set with data both sets have in common"
+      );
+      let dataArray = [];
+      for (let ele of this.storage) {
+        if (set.storage.includes(ele)) {
+          dataArray.push(ele);
+        }
+      }
+      return new SetBuiltIn(dataArray);
+    } else {
+      throw new Error("This set is not an instance of SetBuiltIn");
+    }
+  }
+  difference(set) {
+    if (set instanceof SetBuiltIn) {
+      console.log(
+        "Difference return a new instance of set with data in the first set that are not in the second set"
+      );
+      let dataArray = [];
+      for (let ele of this.storage) {
+        if (!set.storage.includes(ele)) {
+          dataArray.push(ele);
+        }
+      }
+      return new SetBuiltIn(dataArray);
+    } else {
+      throw new Error("This set is not an instance of SetBuiltIn");
+    }
+  }
 }
